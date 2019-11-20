0ce09c45512a00a7dc40af54460f3636de21d625
Me <etienne.arlaud@free.fr>
2019-11-20 15:01:41 +0100

    JL_trajectory -> class

--- isae/tools/trajectory_JL.py
+++ isae/tools/trajectory_JL.py
@@ -2,102 +2,114 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import pi,sqrt,cos,sin
 
-l_sol = 1
-r_turn = 0.1
+def interpol(a, b, phase):
+    return a + phase*(b-a)
 
-height = 0.6
-slope = (height-r_turn)/(0-(-l_sol/2))
-theta_lim3 = np.arctan2(height-r_turn,-l_sol/2)-pi/2
-n_straight = 20
-x_sol = np.linspace(l_sol/2, -l_sol/2,n_straight)
-y_sol = [0 for i in range(len(x_sol))]
+class roundishTriangle:
 
-r_turn1 = r_turn
-x_c1 = x_sol[n_straight-1]
-y_c1 = y_sol[n_straight-1]+r_turn1
-n_turn = 50
-theta_lim1 = np.arctan2(height-r_turn,l_sol/2)-2*pi+pi/2
-theta_turn1 = np.linspace(-pi/2,theta_lim1,n_turn)
-x_turn1 = x_c1+ r_turn1*np.cos(theta_turn1)
-y_turn1 = y_c1+ r_turn1*np.sin(theta_turn1)
+    def __init__(self, length, height, radius):
+        self.l_sol = length  #1
+        self.r_turn = radius #0.1
+        self.height = height
 
-r_turn2 = r_turn
-x_c2 = 0
-y_c2 = height
-theta_turn2 = np.linspace(theta_lim1,theta_lim3-2*pi,n_turn/2)
-x_turn2 = x_c2+ r_turn2*np.cos(theta_turn2)
-y_turn2 = y_c2+ r_turn2*np.sin(theta_turn2)
+        self.slope = (self.height-self.r_turn)/(0-(-self.l_sol/2))
+        self.theta_lim3 = np.arctan2(self.height-self.r_turn,-self.l_sol/2)-pi/2
+        print(self.theta_lim3)
 
-A1 = [x_turn1[n_turn-1],y_turn1[n_turn-1]]
-B1 = [x_turn2[0],y_turn2[0]]
-a_monte = (B1[1]-A1[1])/(B1[0]-A1[0])
-b_monte = A1[1]-a_monte*A1[0]
-x_monte = np.linspace(x_turn1[n_turn-1],x_turn2[0],n_straight)
-y_monte = a_monte*x_monte+b_monte
 
-r_turn3 = r_turn
-x_c3 = x_sol[0]
-y_c3 = y_sol[0]+r_turn3
-theta_turn3 = np.linspace(theta_lim3,-pi/2,n_turn)
-x_turn3 = x_c3+ r_turn3*np.cos(theta_turn3)
-y_turn3 = y_c3+ r_turn3*np.sin(theta_turn3)
+        self.r_turn1 = self.r_turn
+        [xtemp, ytemp] = self.getPos_sol(1)
+        self.x_c1 = xtemp
+        self.y_c1 = ytemp + self.r_turn1
+        
+        self.theta_lim1 = np.arctan2(self.height - self.r_turn, self.l_sol/2)-2*pi+pi/2
 
-A2 = [x_turn2[int(n_turn/2-1)],y_turn2[int(n_turn/2-1)]]
-B2 = [x_turn3[0],y_turn3[0]]
-a_desc = (B2[1]-A2[1])/(B2[0]-A2[0])
-b_desc = A2[1]-a_desc*A2[0]
-x_desc = np.linspace(x_turn2[int(n_turn/2-1)],x_turn3[0],n_straight)
-y_desc = a_desc*x_desc+b_desc
+        self.r_turn2 = self.r_turn
+        self.x_c2 = 0
+        self.y_c2 = self.height
 
-def alpha(x):
-    if x <0.3:
-        prctg = x/0.3
-        x_alpha = x_sol[int(n_straight*prctg)]
+        self.A1 = self.getPos_turn1(1)
+        self.B1 = self.getPos_turn2(0)
+        self.a_monte = (self.B1[1]-self.A1[1])/(self.B1[0]-self.A1[0])
+        self.b_monte = self.A1[1]-self.a_monte*self.A1[0]
+
+        self.r_turn3 = self.r_turn
+        [xtemp, ytemp] = self.getPos_sol(0)
+        self.x_c3 = xtemp
+        self.y_c3 = ytemp + self.r_turn3
+        
+        self.A2 = self.getPos_turn2(1)
+        self.B2 = self.getPos_turn3(0)
+        self.a_desc = (self.B2[1]-self.A2[1])/(self.B2[0]-self.A2[0])
+        self.b_desc = self.A2[1]-self.a_desc*self.A2[0]
+
+    def getPos_sol(self, subphase):
+        x_alpha = interpol(self.l_sol/2, -self.l_sol/2, subphase)
         y_alpha = 0
-    else :
-        if x <0.4:
-            prctg = (x-0.3)/0.1
-            x_alpha = x_turn1[int(n_turn*prctg)]
-            y_alpha = y_turn1[int(n_turn*prctg)]
-        else :
-            if x <0.6:
-                prctg = (x-0.4)/0.2
-                x_alpha = x_monte[int(n_straight*prctg)]
-                y_alpha = y_monte[int(n_straight*prctg)]
-            else :
-                if x <0.7:
-                    prctg = (x-0.6)/0.1
-                    x_alpha = x_turn2[int(n_turn/2*prctg)]
-                    y_alpha = y_turn2[int(n_turn/2*prctg)]
-                else :
-                    if x <0.9:
-                        prctg = (x-0.7)/0.2
-                        x_alpha = x_desc[int(n_straight*prctg)]
-                        y_alpha = y_desc[int(n_straight*prctg)]
-                    else :
-                        prctg = (x-0.9)/0.1
-                        x_alpha = x_turn3[int(n_turn*prctg)]
-                        y_alpha = y_turn3[int(n_turn*prctg)]
-    return [x_alpha,y_alpha]
+        return [x_alpha, y_alpha]
+
+    def getPos_turn1(self, subphase):
+        theta_turn1 = interpol(-pi/2, self.theta_lim1, subphase)
+        x_turn1 = self.x_c1 + self.r_turn1*cos(theta_turn1)
+        y_turn1 = self.y_c1 + self.r_turn1*sin(theta_turn1)
+        return [x_turn1, y_turn1]
+
+    def getPos_turn2(self, subphase):
+        theta_turn2 = interpol(self.theta_lim1, self.theta_lim3-2*pi,subphase)
+        x_turn2 = self.x_c2+ self.r_turn2*cos(theta_turn2)
+        y_turn2 = self.y_c2+ self.r_turn2*sin(theta_turn2)
+        return [x_turn2, y_turn2]
+
+    def getPos_monte(self, subphase):
+        x_monte = interpol(self.A1[0], self.B1[0], subphase)
+        y_monte = self.a_monte*x_monte+self.b_monte
+        return [x_monte, y_monte]
+
+    def getPos_turn3(self, subphase):
+        theta_turn3 = interpol(self.theta_lim3, -pi/2, subphase)
+        x_turn3 = self.x_c3 + self.r_turn3*cos(theta_turn3)
+        y_turn3 = self.y_c3 + self.r_turn3*sin(theta_turn3)
+        return [x_turn3, y_turn3]
+
+    def getPos_desc(self, subphase):
+        x_desc = interpol(self.A2[0], self.B2[0], subphase)
+        y_desc = self.a_desc*x_desc+self.b_desc
+        return [x_desc, y_desc]
+
+    def getPos(self, phase, factor = None):
+        if phase < 0.3:
+            prctg = phase/0.3
+            return self.getPos_sol(prctg)
+
+        if phase < 0.4:
+            prctg = (phase-0.3)/0.1
+            return self.getPos_turn1(prctg)
+
+        if phase < 0.6:
+            prctg = (phase-0.4)/0.2
+            return self.getPos_monte(prctg)
+
+        if phase < 0.7:
+            prctg = (phase-0.6)/0.1
+            return self.getPos_turn2(prctg)
+
+        if phase < 0.9:
+            prctg = (phase-0.7)/0.2
+            return self.getPos_desc(prctg)
+
+        if phase <= 1:
+            prctg = (phase-0.9)/0.1
+            return self.getPos_turn3(prctg)
+        
+        return None
 
 
-plt.plot(x_sol,y_sol,"b--",label="contact")
-plt.plot(x_monte,y_monte,"g--",label="montée")
-plt.plot(x_desc,y_desc,"g--",label="descente")
-plt.plot(x_c1,y_c1,"ro")
-plt.plot(x_turn1,y_turn1,"r--",label="courbe 1")
-plt.plot(x_c2,y_c2,"ro")
-plt.plot(x_turn2,y_turn2,"r--",label="courbe 2")
-plt.plot(x_c3,y_c3,"ro")
-plt.plot(x_turn3,y_turn3,"r--",label="courbe 3")
-
-for i in range(1000):
-    [x,y] = alpha(i/1000.)
-    plt.plot(x,y,"bo")
+rd = roundishTriangle(1,1,0.1)
+for i in range(100):
+    [x,y] = rd.getPos(i/100.)
+    plt.plot(x,y,"b+")
 
 
-plt.xlim(-l_sol, l_sol)
-plt.ylim(-0.1,1)
 plt.title("Trajectoire pied")
 plt.legend()
 plt.xlabel("Position relative du pied")
