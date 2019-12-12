683129981b9579240808ccf9aa312054912a6f71
Thibault <thb.noel@gmail.com>
2019-12-12 21:03:33 +0100

    deprecated : tn_grading_functions

--- isae/optim/tn_grading_functions.py
+++ isae/optim/tn_grading_functions.py
@@ -1,1 +1,17 @@
 import numpy as np
+
+class grading_RMS:
+	def __init__(self):
+		self.grade_total = 0
+
+	def grade(self, q, qdot, qdot_ref, factors, dt):
+		self.grade_total -= np.sum(factors * (qdot - qdot_ref) ** 2) * dt
+
+	def getGrade(self):
+		return self.grade_total
+
+class finalDistGrading:
+    def __init__(self):
+        self.total_grade = 0
+
+    def grade(self, )
