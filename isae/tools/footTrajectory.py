20b27c54a6e288ec6a28994c432f851168b8fe1e
Thibault <thb.noel@gmail.com>
2020-02-04 14:57:40 +0100

    moved phase_offset out of trajectory clasxs

--- isae/tools/footTrajectory.py
+++ isae/tools/footTrajectory.py
@@ -2,27 +2,24 @@ import matplotlib.pyplot as plt
 from math import sqrt
 import numpy as np
 
-# Defines a trajectory with a set of points and an optional phase offset
+# Defines a trajectory with a set of ordered points
 
 class footTrajectory:
-    def __init__(self, points, phaseOffset=0):
+    def __init__(self, points):
         # points : list of size 2 lists
-        # phaseOffset : float in [0,1[, starting point on the trajectory
         # ex for a triang. traj.: points = [[-1,0] , [0,1] , [1,0]]
         self.points = np.array(points)
-        self.phaseOffset = phaseOffset
         self.segLengths = (np.diff(self.points[:,0])**2 + np.diff(self.points[:,1])**2)**.5 # segment lengths, size n
         self.cumulLength = np.concatenate(([0],np.cumsum(self.segLengths))) # cumulated lengths, size n
 
     def getPos(self, phase): # phase in [0,1] over the path
-        phase += self.phaseOffset
         phase %= 1
         phase *= self.cumulLength.max()
         segIndex = np.argwhere(self.cumulLength < phase)
         if len(segIndex > 1):
             segIndex = segIndex[-1]
         else:
-            segIndex = segIndex[0]
+            segIndex = np.array([0])
 
         prevPoint = self.points[segIndex]
         prevPhase = self.cumulLength[segIndex]
