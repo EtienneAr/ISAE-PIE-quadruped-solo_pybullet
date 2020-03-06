1a0adeda1344b01ae763bbe1c641f845d73d7ccf
Me <etienne.arlaud@free.fr>
2020-03-06 14:24:52 +0100

    Massacre code Thomas

--- isae/scripts/run_simulation.py
+++ isae/scripts/run_simulation.py
@@ -7,197 +7,29 @@ sys.path.insert(0, os.getcwd()) # adds current directory to python path
 
 from isae.sim_control.gradedSimulation import *
 from isae.optim.genAlgParam import *
-from isae.control.noiser import *
+from isae.tools.footTrajectoryBezier import *
 
 import multiprocessing
 
 print("Number of cpu : ", multiprocessing.cpu_count())
-
-params = [
-0.4, 	#length
-0.4, 	#height
-0, 	#top_dx
-0.3, 	#end_dX
-0.3, 	#end_dy
-0.0, 	#middle_dx
-0.0, 	#middle_dy
-0.82, 	#onGroundPhase 
-3, #period
-1.5, 	#bodyHeight
-
-
-
-# #0 => -51
-# 0.1404125976121936, 	#length
-# 0.5422511958451965, 	#height
-# 0.08550494008487197, 	#top_dx
-# 0.04994501161726333, 	#end_dX
-# 0.014083287157346037, 	#end_dy
-# 0.2233985102263838, 	#middle_dx
-# 0.17793357475753685, 	#middle_dy
-# 0.6693514715596918, 	#onGroundPhase 
-# 1.2, #period
-# 1.4051279926093874, 	#bodyHeight
-
-
-
-# #0 => -18
-# 0.3061055061406551, 	#length
-# 0.5951045790950397, 	#height
-# 0.31335926932491476, 	#top_dx
-# 0.05316726690076732, 	#end_dX
-# 0.002870177069226343, 	#end_dy
-# -0.1188468558677328, 	#middle_dx
-# 0.06720471734525713, 	#middle_dy
-# 0.7896768078427758, 	#onGroundPhase 
-# 0.5524070191929794, 	#preriod
-# 1.6379950795561544, 	#bodyHeight
-
-
-# TESTED ON ROBOT
-# #0 => -22
-# 0.14563324113943493, 	#length
-# 0.5409825886779587, 	#height
-# -0.06727575508813433, 	#top_dx
-# 0.11977483315736429, 	#end_dX
-# 0.1296427789524941, 	#end_dy
-# -0.17260233493745536, 	#middle_dx
-# 0.06720471734525713, 	#middle_dy
-# 0.7841151011066362, 	#onGroundPhase 
-# 2* 0.6451574599189981, 	#preriod
-# 1.5003555571893732, 	#bodyHeight
-
-
-# #0 => -12
-# 0.13193453226003413, 	#length
-# 1.3512355234987758, 	#height
-# -0.43360866145837307, 	#top_dx
-# 0.08787105222212671, 	#end_dX
-# 0.09274779651763523, 	#end_dy
-# 0.004334391317216024, 	#middle_dx
-# -0.03784137239537205, 	#middle_dy
-# 0.9681577398305765, 	#onGroundPhase 
-# 0.5236698318512114, 	#preriod
-# 1.8242074368232974, 	#bodyHeight
-
-# #0 => -17
-# 0.13979392750638392, 	#length
-# 0.6960755306468621, 	#height
-# -0.08652808293856407, 	#top_dx
-# 0.10801735343812276, 	#end_dX
-# 0.182021280330618, 	#end_dy
-# 0.1311407311902144, 	#middle_dx
-# 0.13654642698410985, 	#middle_dy
-# 0.9400546750111205, 	#onGroundPhase 
-# 0.7420926326981498, 	#preriod
-# 1.5344035227788582, 	#bodyHeight
-
-
-# #0 => -19
-# 0.3676696428008097, 	#length
-# 0.6924094374434756, 	#height
-# -0.44255392600420895, 	#top_dx
-# 0.06582406119114259, 	#end_dX
-# 0.13305972177080083, 	#end_dy
-# -0.33606251922069774, 	#middle_dx
-# 0.0, 					#middle_dy
-# 0.917987629439546, 		#onGroundPhase 
-# 0.7307738251919189, 	#preriod
-# 1.4801748792118907, 	#bodyHeight
-
-
-
-# #0 => -17
-# 0.46145736995143083, 	#length
-# 1.1088043084730161, 	#height
-# -0.767421454708398, 	#top_dx
-# 0.15076494691176015, 	#end_dX
-# -0.22743789422052113, 	#end_dy
-# 0.050391304172427764, 	#middle_dx
-# -0.022606724811983114, 	#middle_dy
-# 0.9300935097409295, 	#onGroundPhase 
-# 0.7434371952924206, 	#preriod
-# 1.581872873912653, 	#bodyHeight
-
-
-# 0.40837721208727384, 	#length
-# 0.9549255241301574, 	#height
-# -0.47767912333829576, 	#top_dx
-# 0.2763327505364216, 	#end_dX
-# -0.2642250029947522, 	#end_dy
-# -0.09014444197440663, 	#middle_dx
-# 0.008976323284597298, 	#middle_dy
-# 0.929566372840192, 	#onGroundPhase 
-# 0.7633821164171444, 	#preriod
-# 1.5696099859696049, 	#bodyHeight
-
-
-# 0.3052912954491256, 	#length
-# 1.0047836625421853, 	#height
-# -0.2274264277819842, 	#top_dx
-# 0.2544160006389065, 	#end_dX
-# -0.17827847838846872, 	#end_dy
-# 0.13082316706267982, 	#middle_dx
-# 0.14996643787054292, 	#middle_dy
-# 0.9061549243444517, 	#onGroundPhase 
-# 0.8905525172820173, 	#preriod
-# 1.4834742296415058, 	#bodyHeight
-
-
-# 0.5896923390427692,
-# 1.2351411441549947,
-# -0.30325369853474826,
-# 0.19495336113966782,
-# -0.1639554165925736,
-# -0.5573880333417938,
-# 0.09797553682313326,
-# 0.9481789012437343,
-# 1.1900672422376402,
-# 1.3300417717994568,
-
-
-# 0.6339712812216678,
-# 0.5763344991926671,
-# -0.6981066810384158,
-# 0.025945223510571658,
-# -0.2206678686188413,
-# 0.1392758586078262,
-# -0.18692625780688682,
-# 0.9404547725673897,
-# 1.3270760294392112,
-# 1.5473986573893712,
-
-# 0.8006669994075996,
-# 0.8043329670481421,
-# -0.078323670192583,
-# 0.138116160023265,
-# -0.15535914805882495,
-# -0.004626636778647319,
-# -0.05041553089667304,
-# 0.8849698258781956,
-# 1.1601537177339414,
-#1.3855528721673105,
-]
 
 # Loop parameters 
 pyb_gui = True
 duration = 80
 
 # Trajectory parameters
-#period = 1.9
-period = params[8]
+period = 0.88
 
-#offsets = [0.0,0.5,0.0,0.5]
 offsets = [0,0.5,0.75,0.25]
 
 # Feet trajectories
+points = [[-0.3625, 0.0, 0.3680, 0.0, 0.2019, 0.4846, -0.4183, 0.5634], [0.1733, 0.0, 0.176, 0.0, -0.2018, 0.1855, -0.2008, -0.1800]]
+footTraj1 = footTrajectoryBezier(points, phaseOffset = offsets[0])
+footTraj2 = footTrajectoryBezier(points, phaseOffset = offsets[1])
+footTraj3 = footTrajectoryBezier(points, phaseOffset = offsets[2])
+footTraj4 = footTrajectoryBezier(points, phaseOffset = offsets[3])
 
-footTraj1 = customTrajectory(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], 0)
-footTraj2 = customTrajectory(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], 0.50)
-footTraj3 = customTrajectory(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], 0.75)
-footTraj4 = customTrajectory(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], 0.25)
-
-bodyHeights = [params[9]] * 4
+bodyHeights = [1.35] * 4
 
 # Geometry and controller
 leg = Leg(1,1)
@@ -230,14 +62,12 @@ traj_log = np.array(traj_log)
 np.save("vertical_main.npy", traj_log)
 
 
-noiseController = noiseIn_noiseOut(robotController, 1 * 100000, positionNoise=0.05, velocityNoise=0, torqueNoise=0.05)
-
 # Create simulation
 walkSim = gradedSimulation()
 
 # Assign parameters to the simulation
 walkSim.setLoopParams(pyb_gui, duration, leg)
-walkSim.setController(noiseController)
+walkSim.setController(robotController)
 
 walkSim.initializeSim()
 
---
+++ isae/tools/footTrajectoryBezier.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+class footTrajectoryBezier:
+	
+    def __init__(self, points, phaseOffset = 0):
+        self.points = points # form : [[x1 y1 x2 y2] , [delta_x1 d_y1 d_x2 d_y2]]  
+                             # d_x1 : delta x for derivative to positionnate points around x1 
+							 # --> assure continuous fonction
+        self.pointsL = []
+        self.phaseOffset = phaseOffset
+
+    #Compute x,y : Bezier poly with 4 control points, t € [0,1]
+    def point_bezier_3(self,points_control,t):
+        x=(1-t)**2
+        y=t*t
+        A = self.combinaison_lineaire(points_control[0],points_control[1],(1-t)*x,3*t*x)
+        B = self.combinaison_lineaire(points_control[2],points_control[3],3*y*(1-t),y*t)
+
+        return np.array([[A[0]+B[0],A[1]+B[1]]])
+
+    def combinaison_lineaire(self,A,B,u,v):
+        return [A[0]*u+B[0]*v,A[1]*u+B[1]*v]
+	
+    def derivToList(self,derivList):
+		#transform a list of points and derivative into list of points
+        List_pt = []
+        params_d = derivList[1]
+        pts = derivList[0]          
+
+        for k in range(len(pts)//2) :   
+            P0_x = pts[2*k]
+            P0_y = pts[2*k+1]
+            P1_x = P0_x + params_d[2*k]
+            P1_y = P0_y + params_d[2*k+1]
+            if(2*k+3>len(pts)):
+                P3_x = pts[0]
+                P3_y = pts[1]
+                P2_x = P3_x - params_d[0]
+                P2_y = P3_y - params_d[1]
+            else:
+                P3_x = pts[2*k+2]
+                P3_y = pts[2*k+3]
+                P2_x = P3_x - params_d[2*k+2]
+                P2_y = P3_y - params_d[2*k+3]
+            List_pt.append([P0_x,P0_y])
+            List_pt.append([P1_x,P1_y])
+            List_pt.append([P2_x,P2_y])
+        P0_x = pts[0]
+        P0_y = pts[1]
+        List_pt.append([P0_x,P0_y])
+			
+        return List_pt
+
+
+    def getPos(self, phase):
+        if(len(self.pointsL) == 0):
+            self.pointsL = self.derivToList(self.points)
+
+        #Contact with the ground
+        phase = (phase + self.phaseOffset) %1
+
+		#Curve separated into segments with 4 control points
+        #Get index of the segment
+        nb_curve = (len(self.pointsL)-1)//3
+        a = 1/nb_curve
+        b = phase // a + 1
+        indice_1 = int(b*3 - 3)
+
+        #Get time inside the segment, t € [0,1]
+        t_ref_poly = (phase % a )*nb_curve	
+
+        points_control = [self.pointsL[indice_1 ], self.pointsL[indice_1 + 1] ,self.pointsL[indice_1 + 2] , self.pointsL[indice_1+ 3] ]
+
+        return self.point_bezier_3(points_control, t_ref_poly)
+    
+    # def plot(self):
+    #     T = np.linspace(0,1,50)
+    #     for elt in T:    
+    #         [x,y] = self.getPos(elt)  
+    #         plt.plot(x,y,'x')
+	
+
+    
+
+
+
